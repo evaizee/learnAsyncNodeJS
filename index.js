4bd711e35bf41087da35f1e1759495dc0cfb6b62
evaizee <sorakeyfaiz@gmail.com>
2017-10-02 07:54:58 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+var express   =    require("express");
+var mysql     =    require('mysql');
+var app       =    express();
+var async = require('async');
+/*
+var connection = mysql.createConnection({
+	host     : '10.10.10.54',
+	user     : 'root',
+	password : 'secret',
+	database : 'elugy',
+});
+*/
+
+function sum(a, b){
+	var total = a + b;
+	return total;
+}
+
+function times(a, b){
+	var total = a * b;
+	return total;
+}
+
+function divide(a, b) {
+	var total = a/b;
+	return total;
+}
+
+async.waterfall(
+    [
+        function(callback) {
+        	var num1 = 3;
+        	var num2 = 4;
+            callback(null, num1, sum(num1, num2));
+        },
+        function(arg1, arg2, callback) {
+            console.log(arg1+' '+arg2);
+            callback(null, times(arg1, arg2), arg2);
+        },
+        function(arg1, arg2, callback) {
+            console.log(arg1+' '+arg2);
+            callback(null, divide(arg1, arg2));
+        },
+        function(argument, callback) {
+        	console.log('tes '+argument);
+        	callback(null, argument);
+        }
+    ],
+    function (err, caption) {
+        console.log(caption);
+        // Node.js and JavaScript Rock!
+    }
+);
